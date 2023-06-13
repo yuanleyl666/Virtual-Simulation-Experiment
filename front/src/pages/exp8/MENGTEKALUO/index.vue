ecc95f10c20eab1c9813fb427c2f86f5eb161c28
chrlott <1811130731@qq.com>
2023-06-13 17:18:22 +0800

    修改主页样式

--- front/src/pages/exp8/MENGTEKALUO/index.vue
+++ front/src/pages/exp8/MENGTEKALUO/index.vue
@@ -1,4 +1,26 @@
 <template>
+    <div style="padding-top:60px;padding-bottom:20px">
+        <a-config-provider :locale="locale">
+            <p style="line-height:200%;font-size: 16px;">
+                <a-row justify="center">
+                    <a-col span="6">课程名称：软件工程经济学</a-col>
+                    <a-col span="6">课号：420279</a-col>
+                    <a-col span="6">实验项目名称：蒙特卡洛法实验</a-col>
+                </a-row>
+                <a-row justify="center">
+                    <a-col span="6">实验时间：<span style="border-bottom: 1px solid grey;border-radius: none;"><a-date-picker
+                                v-model="experimentdate" :bordered="false"
+                                style="width:150px;padding-left:3px;padding-right:3px;"
+                                placeholder="点击选择实验时间" /></span></a-col>
+                    <a-col span="6">实验报告人： <span style="border-bottom: 1px solid grey;border-radius: none;"><a-input
+                                v-model="reportername" placeholder="请输入报告人姓名" size="small" :bordered="false"
+                                style="width:18vh;"></a-input></span>
+                    </a-col>
+                    <a-col span="6"></a-col>
+                </a-row>
+            </p>
+        </a-config-provider>
+    </div>
     <h2>一、实验目的 </h2>
     <p class="content"> 本实验使用蒙特卡洛模拟方法对项目进行定量风险分析，通过模拟大量实验并统计实验结果，最终得出概率与累积概率分布图，帮助合理规划项目进度。
     </p>
@@ -153,6 +175,8 @@ export default {
             percent30: ref<number>(0),
             percent15: ref<number>(0),
             editingKey: '',
+            experimentdate: 0,//实验时间
+            reportername: '',//实验人姓名
             editableData: {},
             columns1: [
                 {
